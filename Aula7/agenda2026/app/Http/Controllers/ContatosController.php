6bd417e8b048bc418701fa2c7c06df0fca7a056e
Johann Malkowski <johannmalkowski2006@gmail.com>
2026-04-28 21:36:13 -0300

    Realizado aula10 na pasta aula7

--- Aula7/agenda2026/app/Http/Controllers/ContatosController.php
+++ Aula7/agenda2026/app/Http/Controllers/ContatosController.php
@@ -16,12 +16,26 @@ class ContatosController extends Controller
 
     public function create()
     {
-        
+        return view('contatos.create');
     }
 
     public function store(Request $request)
     {
-        
+        $request->validate(['nome' => 'required|string|max:255',
+        'email' =>'required|email|max:255',
+        'telefone' => 'required|string|max:20',
+        ]);
+        $contato = new Contato();
+        $contato->nome = $request->input('nome');
+        $contato->email = $request->input('email');
+        $contato->telefone = $request->input('telefone');
+        $contato->cidade = $request->input('cidade');
+        $contato->estado = $request->input('estado');
+        if ($contato->save()) {
+            return redirect()->route('contatos.index')->with('success', 'Contato criado com sucesso.');
+        } else {
+            return redirect()->route('contatos.create')->with('error', 'Erro ao criar contato. Tente novamente.');
+        }
     }
 
     public function show(string $id)
--- Aula7/agenda2026/resources/views/contatos/index.blade.php
+++ Aula7/agenda2026/resources/views/contatos/index.blade.php
@@ -1,8 +1,23 @@
-<header>    
+<header>
     <h1>Lista de Contatos</h1>
 </header>
+
 <div>
-    @foreach($contatos as $contato)
-        <p><a href="{{route('contatos.show', $contato->id)}}"> {{$contato->id}} - {{$contato->nome}} - {{$contato->email}}</a></p> 
+    <a href="{{ route('contatos.create') }}">Novo Contato</a>
+</div>
+
+<div style="margin: 20px 0px;">
+    @if (session('success'))
+        <p style="color: green;">{{ session('success') }}</p>
+    @endif
+    @if (session('error'))
+        <p style="color: red;">{{ session('error') }}</p>
+    @endif
+</div>
+
+<div>
+    @foreach ($contatos as $contato)
+        <p><a href="{{ route('contatos.show', $contato->id) }}">{{ $contato->id }} - {{ $contato->nome }} - {{ $contato->email }}</a></p>
     @endforeach
 </div>
+
